8380e1305f6a0e86dbee8545b297f93faaec88ba
jijiseong <jiseong5036@gmail.com>
2024-02-17 00:55:44 +0900

    setting: pwa 환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Vital Routes',
+        short_name: 'Vital Routes',
+        description: 'Vital Routes',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/vite.svg',
+            type: 'image/svg+xml',
+            sizes: '192x192',
+          },
+        ],
+      },
+    }),
+  ],
 });

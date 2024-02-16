4c6c06c1687c43d3948a66b427e0b5aba003b286
jijiseong <jiseong5036@gmail.com>
2024-02-17 00:31:05 +0900

    setting: eslint, prettier 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-/** airbnb 적용 이후 해당 코드에서 에러발생하여 아래와 같이 수정
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
-*/
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
- )
+);

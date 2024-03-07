0a22ec82b6f0117d978b9ec65561b09e79b151d9
jijiseong <jiseong5036@gmail.com>
2024-03-07 17:25:19 +0900

    setting: react-query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Outlet, ScrollRestoration } from 'react-router-dom';
 import Layout from '@/components/units/Layout';
+import QueryProvider from '@/app/QueryProvider';
 
 function App() {
   return (
-    <>
+    <QueryProvider>
       <ScrollRestoration />
       <Layout>
         <Outlet />
       </Layout>
-    </>
+    </QueryProvider>
   );
 }
 

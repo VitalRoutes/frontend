1dfd77d8868d883f197b99ade0717c0d3dd20848
jijiseong <jiseong5036@gmail.com>
2024-03-13 22:22:25 +0900

    feat: profile UI

--- src/pages/profile/components/FilterSection.tsx
+++ src/pages/profile/components/FilterSection.tsx
@@ -2,20 +2,37 @@ import Button from '@/components/common/Button';
 import { useFilterStore } from '@/store/profileStore';
 
 function FilterSection() {
-  const { setFilter } = useFilterStore();
+  const ACTIVE_COLOR = 'bg-gray-1 text-gray-8';
+  const { filter, setFilter } = useFilterStore();
 
   return (
     <div className="flex items-center justify-center gap-[16px]">
-      <Button variant="tab-a" onClick={() => setFilter('myChallenge')}>
+      <Button
+        variant="tab-a"
+        className={filter === 'myChallenge' ? ACTIVE_COLOR : undefined}
+        onClick={() => setFilter('myChallenge')}
+      >
         나의 챌린지
       </Button>
-      <Button variant="tab-a" onClick={() => setFilter('participated')}>
+      <Button
+        variant="tab-a"
+        className={filter === 'participated' ? ACTIVE_COLOR : undefined}
+        onClick={() => setFilter('participated')}
+      >
         참여 중인 챌린지
       </Button>
-      <Button variant="tab-a" onClick={() => setFilter('liked')}>
+      <Button
+        variant="tab-a"
+        className={filter === 'liked' ? ACTIVE_COLOR : undefined}
+        onClick={() => setFilter('liked')}
+      >
         좋아요한 챌린지
       </Button>
-      <Button variant="tab-a" onClick={() => setFilter('bookmarked')}>
+      <Button
+        variant="tab-a"
+        className={filter === 'bookmarked' ? ACTIVE_COLOR : undefined}
+        onClick={() => setFilter('bookmarked')}
+      >
         북마크한 챌린지
       </Button>
     </div>

15ff8c96d5d3bee8458db622007a86dba49cd694
jijiseong <jiseong5036@gmail.com>
2024-03-14 00:32:01 +0900

    setting: favicon, 앱아이콘 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,35 @@ import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Vital Routes',
+        short_name: 'Vital Routes',
+        description: 'Vital Routes',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './public/logo-black-512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+          {
+            src: './public/logo-black-192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },

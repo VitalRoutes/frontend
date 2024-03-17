0cd5b62dc51fbee44f9d0468c8725ee110a8eaac
jijiseong <jiseong5036@gmail.com>
2024-03-17 22:42:08 +0900

    design: 컨텐츠 없을때 목록 디자인

--- src/pages/challenge/components/ChallengeListSection/index.tsx
+++ src/pages/challenge/components/ChallengeListSection/index.tsx
@@ -1,8 +1,9 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { getImageUrl } from '@/utils/getImageUrl';
 import ChallengeCard from '@/components/units/ChallengeCard';
 import useChallengeList from '@/hooks/challenge/useChallengeList';
 import BearLoading from '@/components/common/Loading/BearLoading';
+import Button from '@/components/common/Button';
 
 function ChallengeListSection() {
   const { data, isLoading } = useChallengeList();
@@ -14,7 +15,19 @@ function ChallengeListSection() {
 
   if (!data) {
     return (
-      <img src={getImageUrl('picture/none_contents.png')} alt="none-contents" />
+      <div className="flex flex-col items-center gap-[62px]">
+        <div className="flex flex-col items-center gap-[16px]">
+          <img
+            className="mb-[16px]"
+            src={getImageUrl('picture/none_contents.png')}
+            alt="none-contents"
+          />
+          <div className="text-[24px] font-bold">등록된 챌린지가 없습니다.</div>
+        </div>
+        <Link to="/challenge/registration">
+          <Button variant="third-c">챌린지 등록하러 가기</Button>
+        </Link>
+      </div>
     );
   }
 
